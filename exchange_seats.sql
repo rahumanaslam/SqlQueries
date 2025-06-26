98fa094186d3f6c90fa14105304e903d4c0fb827
rahumanaslam <22251571+rahumanaslam@users.noreply.github.com>
2025-06-26 10:33:21 -0400

    Add SQL query

--- exchange_seats.sql
+++ exchange_seats.sql
@@ -10,3 +10,14 @@ SELECT(
     student 
 FROM Seat
 ORDER BY id;
+
+-- Alternative : MOD()
+SELECT
+    CASE 
+        WHEN MOD(id,2) = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+        WHEN MOD(id,2) = 1 THEN id + 1
+        WHEN MOD(id,2) = 0 THEN id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
